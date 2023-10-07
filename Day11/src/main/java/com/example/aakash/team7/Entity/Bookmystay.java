e835cf10dfb78b3f2ba3bb5f546df1c6776e2645
Aakashsakthi12 <120013331+Aakashsakthi12@users.noreply.github.com>
2023-10-07 12:44:03 +0530

    Add files via upload

---
+++ Day11/src/main/java/com/example/aakash/team7/Entity/Bookmystay.java
@@ -0,0 +1,66 @@
+package com.example.aakash.team7.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sign") // Corrected table name
+public class Bookmystay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Update field names to match the column names in your database table
+    private String firstname; // Match the column name "firstname"
+    private String lastname;  // Match the column name "lastname"
+    private String email;
+    private String password;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Bookmystay(Long id, String firstname, String lastname, String email, String password) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.password = password;
+	}
+	public Bookmystay() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+    
+	
+}
---
+++ Day11/src/main/java/com/example/aakash/team7/Service/BookmystayService.java
@@ -0,0 +1,33 @@
+package com.example.aakash.team7.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aakash.team7.Entity.Bookmystay;
+import com.example.aakash.team7.Repository.BookmystayRepository;
+@Service
+public class BookmystayService {
+
+	@Autowired
+	BookmystayRepository bookmystayRepository;
+	
+	public List<Bookmystay> getAllBookmystay() {
+		return bookmystayRepository.findAll();
+	}
+	
+	public Bookmystay getbookmystay(Long bookmystayId) {
+		Optional<Bookmystay> bookmystayOptional = bookmystayRepository.findById(bookmystayId);
+		return bookmystayOptional.orElse(null);
+	}
+	
+	public Bookmystay savebookmystay(Bookmystay bookmystay) {
+		return bookmystayRepository.save(bookmystay);
+	}
+	
+	public void deletebookmystay(Long bookmystayId) {
+		bookmystayRepository.deleteById(bookmystayId);
+	}
+}
