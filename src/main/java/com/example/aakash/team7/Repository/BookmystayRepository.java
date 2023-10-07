d876732c97f61497cf20f0558fd0ef3278fe8d86
Aakashsakthi12 <120013331+Aakashsakthi12@users.noreply.github.com>
2023-10-07 12:43:20 +0530

    Delete src directory

--- src/main/java/com/example/aakash/team7/Repository/BookmystayRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aakash.team7.Repository;
-
-import com.example.aakash.team7.Entity.Bookmystay; // Import your entity class here
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmystayRepository extends JpaRepository<Bookmystay, Long> {
-}
